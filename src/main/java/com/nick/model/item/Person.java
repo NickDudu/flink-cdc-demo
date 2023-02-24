53c961096100534d9a9db04f3521bf2dea710e14
Hu <huxiaod@08f8bc6ba193.ant.amazon.com>
2023-02-24 21:07:05 +0800

    first commit

---
+++ src/main/java/com/nick/model/item/Person.java
@@ -0,0 +1,34 @@
+package com.nick.model.item;
+
+public class Person {
+    public String customerId;
+    public String sex;
+    public int age;
+    public int birthMonth;
+    public int birthDay;
+    public int openDays;
+    public int totalPoints;
+    public String province;
+    public String membership;
+
+    public Person(String customerId,
+            String sex,
+            int age,
+            int birthMonth,
+            int birthDay,
+            int openDays,
+            int totalPoints,
+            String province,
+            String membership) {
+        this.customerId = customerId;
+        this.sex = sex;
+        this.age = age;
+        this.birthMonth = birthMonth;
+        this.birthDay = birthDay;
+        this.openDays = openDays;
+        this.totalPoints = totalPoints;
+        this.province = province;
+        this.membership = membership;
+    }
+
+}
